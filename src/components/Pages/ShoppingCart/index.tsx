1e90f0e4284877c9b04de4d42c23229e17ed39fb
Anton Borisov <anton.borisov.08@gmail.com>
2021-02-08 22:41:05 +0400

    changed url

--- src/components/Pages/ShoppingCart/index.tsx
+++ src/components/Pages/ShoppingCart/index.tsx
@@ -11,7 +11,7 @@ const ShoppingCart = ({}: CartProps) => {
 
     useEffect(() => {
         setLoading(true)
-        fetch("./online-store/db.json",{
+        fetch("./db.json",{
             headers : {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
